c8052d8ade58615eebaaeb9fda7ad2b3c161f543
Edison Lizano <elizano@konradgroup.com>
2016-03-30 15:31:24 -0600

    add whaeter icons

--- www/app/settings/settings.controller.js
+++ www/app/settings/settings.controller.js
@@ -8,6 +8,27 @@
   function SettingsController(conection){
     var settings = this;
 
+    settings.location = 'Kiev';
+    settings.autoDefine = true;
+    settings.notification = true;
+    settings.unit;
+
+
+    settings.temp = function(unit) {
+      if(unit == 'c') {
+        //sets celcious
+      }
+
+      if(unit == 'f') {
+        //sets farengh
+      }
+
+    }
+
+    settings.setValues = function(){
+      conection.setSettings(settings.location, settings.autoDefine, settings.notification, settings.time, settings.unit);
+    }
+
 
   }
 
