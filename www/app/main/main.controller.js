234d38ab6e9414ac9e596bbc0a078bc0dd9abf61
MilloEscobar <milloescobar@gmail.com>
2016-03-31 10:41:55 -0600

    tast orientation

--- www/app/main/main.controller.js
+++ www/app/main/main.controller.js
@@ -10,16 +10,6 @@
     main.connection = conection;
     main.title = 'Test main controller';
 
-
-  // settings.location = location;
-  // settings.autoDefine = autoDefine;
-  // settings.notification = notification;
-  // settings.time = time;
-  // settings.unit = unit;
-  //
-
-
-
     main.getWeather = function () {
       main.connection.getWeather(main.connection.settings.location , main.connection.settings.unit)
       .then(function (res){
@@ -27,56 +17,25 @@
       });
     }();
 
-    document.addEventListener("deviceready", function () {
+  main.dir = 0;
 
-      $cordovaDeviceOrientation.getCurrentHeading().then(function(result) {
-         var magneticHeading = result.magneticHeading;
-         var trueHeading = result.trueHeading;
-         var accuracy = result.headingAccuracy;
-         var timeStamp = result.timestamp;
-         console.log('success');
-         alert('38');
-      }, function(err) {
-        // An error occurred
-        console.log('error');
-      });
+    var options = {
+        frequency: 500
+    };
 
+    main.watchPromise = $cordovaDeviceOrientation.watchHeading(options);
 
-
-      var options = {
-        frequency: 3000,
-        filter: true     // if frequency is set, filter is ignored
-      }
-
-      var watch = $cordovaDeviceOrientation.watchHeading(options).then(
-        null,
-        function(error) {
-          // An error occurred
-          console.log('error');
-        },
-        function(result) {   // updates constantly (depending on frequency value)
-          var magneticHeading = result.magneticHeading;
-          var trueHeading = result.trueHeading;
-          var accuracy = result.headingAccuracy;
-          var timeStamp = result.timestamp;
-          console.log('success');
-          alert('63');
-        });
-
-
-      watch.clearWatch();
-      // OR
-      $cordovaDeviceOrientation.clearWatch(watch)
-        .then(function(result) {
-          // Success!
-          console.log('success');
-          alert('73');
-        }, function(err) {
-          // An error occurred
-          console.log('error');
-        });
-
-    }, false);
+    main.watchPromise.then(
+            null, 
+            function(error) {
+                console.log(error);
+                alert("no");
+            }, 
+            function(result) {
+                main.dir = result.trueHeading;
+                alert("yes");
+            }
+        );
 
   }
 
