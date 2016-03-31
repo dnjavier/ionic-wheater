f61c945615ea8fdde08f6cdf601c954fac4d277d
Edison Lizano <elizano@konradgroup.com>
2016-03-31 08:18:20 -0600

    change unit temp color

--- www/app/settings/settings.controller.js
+++ www/app/settings/settings.controller.js
@@ -7,6 +7,7 @@
 
   function SettingsController(conection){
     var settings = this;
+    settings.unitColor = true;
 
     settings.location = conection.settings.location;
     settings.autoDefine = conection.settings.autoDefine;
@@ -18,12 +19,14 @@
     settings.temp = function(unit) {
       if(unit == 'c') {
         settings.unit = 'metric';
+        settings.unitColor = true;
       }
 
       if(unit == 'f') {
         settings.unit = 'imperial';
+        settings.unitColor = false;
       }
-
+      console.log(settings.unitColor);
     }
 
     settings.setValues = function(){
